94cf2daf1536d9acd82340f36ea4844c907de6b0
taic-dev <c.t.o.taishi.0530@gmail.com>
2022-04-29 01:01:46 +0900

    答えと入力値が一致しているか

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const commandAnswer = document.getElementById("command-answer");
 const commandArea = document.getElementById("command");
+let command = "console.log()";
 
-let answer = "console.log";
-answer = [...answer];
-console.log(answer);
-commandAnswer.innerHTML = "console.log()";
-let typingResult = "";
+// const commandSelect = (command) => {
+    let answer = command;
+    let answerLength = answer.length;
+    console.log(answerLength);
+    answer = [...answer];
+    commandAnswer.innerHTML = command;
+    let typingResult = "";
+    let count = 0;
+// }
+// commandSelect(command);
 window.addEventListener("keydown",(e)=>{
-    console.log(answer[1]);
-    console.log(e.key);
-    typingResult += e.key
-    commandArea.innerHTML = typingResult;
+    if(answerLength > count){
+            if(answer[count] == e.key || "Shift" == e.key){
+                if("Shift" == e.key) return;
+                typingResult += e.key
+                commandArea.innerHTML = typingResult;
+                commandAnswer.classList.remove("bg--red");
+                count++
+            }else{
+                console.log(answer[count]);
+                console.log(e.key)
+                console.log("失敗");
+                commandAnswer.classList.add("bg--red");
+            }        
+    }else{
+        command = "change";
+        console.log(command)
+        // commandSelect();
+    }
 });
