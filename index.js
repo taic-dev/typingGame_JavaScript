6ce06732d995f94a094d8a90d026f1f6daab05b3
taic-dev <c.t.o.taishi.0530@gmail.com>
2022-04-29 09:12:50 +0900

    複数のコマンド表示

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
 const commandAnswer = document.getElementById("command-answer");
 const commandArea = document.getElementById("command");
-let command = "console.log()";
-
+let command = ["console.log()","getElementById","querySelector"];
+let commandRand = Math.floor(Math.random()*command.length)
+console.log(commandRand)
 // const commandSelect = (command) => {
-    let answer = command;
+    let answer = command[commandRand];
+    commandAnswer.innerHTML = answer;
+    console.log(answer)
     let answerLength = answer.length;
     console.log(answerLength);
     answer = [...answer];
-    commandAnswer.innerHTML = command;
     let typingResult = "";
     let count = 0;
 // }
-// commandSelect(command);
-window.addEventListener("keydown",(e)=>{
+
+window.addEventListener("keyup",(e)=>{
+    // commandSelect(command);
     if(answerLength > count){
-            if(answer[count] == e.key || "Shift" == e.key){
-                if("Shift" == e.key) return;
-                typingResult += e.key
-                commandArea.innerHTML = typingResult;
-                commandAnswer.classList.remove("bg--red");
-                count++
-            }else{
-                console.log(answer[count]);
-                console.log(e.key)
-                console.log("失敗");
-                commandAnswer.classList.add("bg--red");
-            }        
+        if(answer[count] == e.key || "Shift" == e.key){
+            if("Shift" == e.key) return;
+            typingResult += e.key
+            commandArea.innerHTML = typingResult;
+            commandAnswer.classList.remove("bg--red");
+            count++
+        }else{
+            console.log(answer[count]);
+            console.log(e.key)
+            console.log("失敗");
+            commandAnswer.classList.add("bg--red");
+        }        
     }else{
-        command = "change";
-        console.log(command)
-        // commandSelect();
+        commandAnswer.innerHTML = "";
+        commandArea.innerHTML = "";
+        answer = command[1];
+        commandAnswer.innerHTML = answer;
+        console.log(commandRand)
+        // answer = command[commandRand];
+        // console.log(answer)
+        answerLength = answer.length;
+        console.log(answerLength);
+        answer = [...answer];
+        typingResult = "";
+        count = 0;
     }
 });
+
+// 初期化処理を作成する
