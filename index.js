81198b5176e65e5b302efefa5b8ccc51687a36b8
taic-dev <c.t.o.taishi.0530@gmail.com>
2022-05-01 10:37:50 +0900

    タイピング機能_完成

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const commandAnswer = document.getElementById("command-answer");
 const commandArea = document.getElementById("command");
-let command = ["console.log()","getElementById","querySelector"];
+let command = [
+    "console.log()",
+    "getElementById()",
+    "querySelector()",
+    "addEventListener()",
+    "isArray()"
+];
 let commandRand = Math.floor(Math.random()*command.length)
 console.log(commandRand)
-// const commandSelect = (command) => {
     let answer = command[commandRand];
     commandAnswer.innerHTML = answer;
-    console.log(answer)
     let answerLength = answer.length;
+    console.log(answer)
     console.log(answerLength);
     answer = [...answer];
     let typingResult = "";
     let count = 0;
-// }
+    let typoCount = 0;
 
-window.addEventListener("keyup",(e)=>{
-    // commandSelect(command);
+
+window.addEventListener("keydown",(e)=>{
     if(answerLength > count){
         if(answer[count] == e.key || "Shift" == e.key){
             if("Shift" == e.key) return;
@@ -23,26 +28,32 @@ window.addEventListener("keyup",(e)=>{
             commandArea.innerHTML = typingResult;
             commandAnswer.classList.remove("bg--red");
             count++
+            if(answerLength == count){
+                commandAnswer.innerHTML = "";
+                commandArea.innerHTML = "";
+                command = ["console.log()","getElementById()","querySelector()","addEventListener()","isArray()"];
+                commandRand = Math.floor(Math.random()*command.length)
+                answer = command[commandRand];
+                commandAnswer.innerHTML = answer;
+                console.log(commandRand)
+                // answer = command[commandRand];
+                // console.log(answer)
+                answerLength = answer.length;
+                console.log(answerLength);
+                answer = [...answer];
+                typingResult = "";
+                count = 0;
+            }
         }else{
-            console.log(answer[count]);
-            console.log(e.key)
+            typoCount++
+            console.log(typoCount)
             console.log("失敗");
             commandAnswer.classList.add("bg--red");
         }        
-    }else{
-        commandAnswer.innerHTML = "";
-        commandArea.innerHTML = "";
-        answer = command[1];
-        commandAnswer.innerHTML = answer;
-        console.log(commandRand)
-        // answer = command[commandRand];
-        // console.log(answer)
-        answerLength = answer.length;
-        console.log(answerLength);
-        answer = [...answer];
-        typingResult = "";
-        count = 0;
     }
 });
 
 // 初期化処理を作成する
+// オープニング
+// タイポカウント
+// タイム計測
